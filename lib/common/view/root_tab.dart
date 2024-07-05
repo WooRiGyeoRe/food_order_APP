c84be9b3bfa67f2dabdbb5bfb663cc1d443c2cc3
WooRiGyeoRe <rufp0607@naver.com>
2024-07-05 20:01:03 +0900

    바텀네비바 탭 완성

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,9 +9,41 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  // 탭 컨트롤러
+  // late => 나중에 이 값이 입력될 건데, 이 값을 부를 때 이 컨트롤러라는 값이 이미 선언 돼 있을 거야 가정
+  // TabController? 가능... 하지만 controller 쓸 때마다 null 처리해야 함
+  late TabController controller;
+
   // 탭 저장
   int index = 0; // 처음에는 0
+
+  @override
+  void initState() {
+    super.initState();
+
+    // length = 몇개의 화면을 컨트롤할 건지
+    // vsync에는 무조건 with SingleTickerProviderStateMixin 작성하고 현재 class를 작성
+    controller = TabController(length: 4, vsync: this);
+
+    // addListener => 값이 변경될 때마다 특정 변수를 실행해라
+    controller.addListener(tabListener);
+  }
+
+  // 컨트롤러의 인덱스를 이 인덱스에다가 계속 넣어줌
+  // 그리고 매번 이 컨트롤러에서 변화가 있을 때마다 setState를 실행
+  // controller에서 매번 속성들이 바뀔 때마다 tabListener 함수가 실행됨
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +60,8 @@ class _RootTabState extends State<RootTab> {
         // 클릭한 탭의 인덱스 숫자
         onTap: (int index) {
           setState(() {
-            this.index = index; // 선택할 때마다 this.index는 index로 저장
+            // this.index = index; // 선택할 때마다 this.index는 index로 저장
+            controller.animateTo(index);
           });
         },
         currentIndex: index, // 현재 선택된 인덱스 지정
@@ -51,8 +84,14 @@ class _RootTabState extends State<RootTab> {
           ),
         ],
       ),
-      child: const Center(
-        child: Text('Root Tab'),
+      child: TabBarView(
+        controller: controller,
+        children: [
+          Center(child: Container(child: const Text('홈'))),
+          Center(child: Container(child: const Text('음식'))),
+          Center(child: Container(child: const Text('주문'))),
+          Center(child: Container(child: const Text('프로필'))),
+        ],
       ),
     );
   }
